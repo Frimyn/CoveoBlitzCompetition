e94ef942c48f9bdc0411a522b751d4bb4fa98fc5
Marie-Philippe Brassard <mariephilippe.brassard@gmail.com>
2024-01-13 15:21:15 -0500

    rotation vaisseau

--- python/bot.py
+++ python/bot.py
@@ -1,13 +1,18 @@
 from game_message import *
 from crew_movement import *
-from danger_detection import *
+# from danger_detection import *
 from actions import *
 import random
+import time
 
 class Bot:
     def __init__(self):
         print("Initializing your super mega duper bot")
         self.crew_movement = CrewMovement()
+        self.rotation_duration = 4.5  # Durée de rotation dans chaque sens en secondes
+        self.last_rotation_time = time.time()
+        self.rotation_direction = 1  # 1 pour sens horaire, -1 pour sens anti-horaire
+        self.first_rotation = 2  # Indique si c'est la première rotation
 
     def get_next_move(self, game_message: GameMessage):
         actions = []
@@ -16,18 +21,6 @@ class Bot:
         my_ship = game_message.ships.get(team_id)
         other_ships_ids = [shipId for shipId in game_message.shipsPositions.keys() if shipId != team_id]
 
-        # # Find who's not doing anything and try to give them a job?
-        # idle_crewmates = [crewmate for crewmate in my_ship.crew if crewmate.currentStation is None and crewmate.destination is None]
-
-        # if len(idle_crewmates) == len(my_ship.crew):
-        #     actions.extend(self.crew_movement.send_crew_to_battlestations(game_message))
-            
-        # for crewmate in idle_crewmates:
-        #     visitable_stations = crewmate.distanceFromStations.shields + crewmate.distanceFromStations.turrets + crewmate.distanceFromStations.helms + crewmate.distanceFromStations.radars
-        #     station_to_move_to = random.choice(visitable_stations)
-        #     actions.append(CrewMoveAction(crewmate.id, station_to_move_to.stationPosition))
-
-        # Now crew members at stations should do something!
         operatedTurretStations = [station for station in my_ship.stations.turrets if station.operator is not None]
         for turret_station in operatedTurretStations:
             possible_actions = [
@@ -44,8 +37,28 @@ class Bot:
             actions.append(random.choice(possible_actions))
 
         operatedHelmStation = [station for station in my_ship.stations.helms if station.operator is not None]
+        
         if operatedHelmStation:
-            actions.append(ShipRotateAction(45.0))
+            current_time = time.time()
+            elapsed_time_since_rotation = current_time - self.last_rotation_time
+
+            # Vérifiez si c'est la première rotation
+            if self.first_rotation > 0:
+                # Inversez la direction de rotation à la moitié du temps déterminé
+                if elapsed_time_since_rotation >= 2:
+                    self.rotation_direction *= -1
+                    self.first_rotation -= 1  # La première rotation est terminée
+                    self.last_rotation_time = current_time
+
+            # Vérifiez si le temps écoulé depuis la dernière rotation a dépassé la durée de rotation
+            if elapsed_time_since_rotation >= self.rotation_duration:
+                # Inversez la direction de rotation et réinitialisez le temps de la dernière rotation
+                self.rotation_direction *= -1
+                self.last_rotation_time = current_time
+
+            # Effectuez la rotation en fonction de la direction actuelle
+            rotation_angle = 45.0 * self.rotation_direction
+            actions.append(ShipRotateAction(rotation_angle))
 
         operatedRadarStation = [station for station in my_ship.stations.radars if station.operator is not None]
         for radar_station in operatedRadarStation:
@@ -55,5 +68,4 @@ class Bot:
         crew_actions = self.crew_movement.send_crew_to_battlestations(game_message)
         actions.extend(crew_actions)
 
-        # You can clearly do better than the random actions above! Have fun!
         return actions
